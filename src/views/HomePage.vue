d60e15190a22a2e47f7ab0a03bcdd55fe36a8e15
PeterJakobs1 <peter.bj@live.no>
2022-09-08 12:33:15 +0200

    fixed unshift method

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,68 +1,61 @@
+
+<script setup lang="ts">
+import { IonContent, IonButtons, IonButton, IonCardContent, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCard, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { ref } from 'vue';
+
+const campingSpots = ref([
+  {
+    id: 1,
+      title: "Fin plass ved Ulsrudvann",
+      description:"fant denne da jeg gikk kveldstur rundt vannet",
+      hashtags:"#oslo #østmarka #nærtur",
+      imgUrl: "https://images.squarespace-cdn.com/content/v1/5283c6b4e4b02d47ac471135/1535888882553-S2T8J0R5TGRA3AYMO67A/Telttur-1500.jpg"
+  }
+]);
+
+const addCampingSpot = () => {
+  const newCampingSpot = {
+    id: 2,
+          title: "Fin plass på føylen",
+          description:"Ble tipset av en god kompis!",
+          hashtags:"#bergen #fløyen #7fjell",
+          imgUrl: "https://cdn.dnt.org/prod/sherpa/fotoweb/9d5def8a-4c6f-4021-96ba-6c2bb4d38588.jpg"
+  }
+
+  campingSpots.value.unshift(newCampingSpot);
+}
+
+</script>
+  
+
 <template>
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>Baal 🏕</ion-title>
+        <ion-buttons slot="end">
+          <ion-button @click="addCampingSpot">+</ion-button>
+        </ion-buttons>
       </ion-toolbar>
     </ion-header>
-    
+
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
+
+      <ion-card v-for="spot in campingSpots" :key="spot.id">
+        <img :src="spot.imgUrl" />
+        <ion-card-header>
+          <ion-card-subtitle>{{ spot.hashtags }}</ion-card-subtitle>
+          <ion-card-title>{{ spot.title }}</ion-card-title>
+        </ion-card-header>
+
+        <ion-card-content>
+          {{ spot.description }}
+        </ion-card-content>
+
+      </ion-card>
+
+
     </ion-content>
   </ion-page>
 </template>
 
-<script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-  name: 'HomePage',
-  components: {
-    IonContent,
-    IonHeader,
-    IonPage,
-    IonTitle,
-    IonToolbar
-  }
-});
-</script>
-
-<style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
-</style>
